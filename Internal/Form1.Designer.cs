a077ae6feac3d43ff9025c5588d11c41a91a62b6
Cooper Stewart <cooperstewart@mmc.school.nz>
2020-08-03 13:04:04 +1200

    Implemented gravity but it does not stop at the ground

--- Internal/Form1.Designer.cs
+++ Internal/Form1.Designer.cs
@@ -28,21 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.gameDesigner = new System.Windows.Forms.Label();
+            this.endText1 = new System.Windows.Forms.Label();
+            this.scoreText = new System.Windows.Forms.Label();
+            this.endText2 = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.Titan = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.pipeTop = new System.Windows.Forms.PictureBox();
+            this.pipeBottom = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Titan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).BeginInit();
             this.SuspendLayout();
+            // 
+            // gameDesigner
+            // 
+            this.gameDesigner.AutoSize = true;
+            this.gameDesigner.Location = new System.Drawing.Point(375, 111);
+            this.gameDesigner.Name = "gameDesigner";
+            this.gameDesigner.Size = new System.Drawing.Size(35, 13);
+            this.gameDesigner.TabIndex = 0;
+            this.gameDesigner.Text = "label1";
+            // 
+            // endText1
+            // 
+            this.endText1.AutoSize = true;
+            this.endText1.Location = new System.Drawing.Point(79, 99);
+            this.endText1.Name = "endText1";
+            this.endText1.Size = new System.Drawing.Size(35, 13);
+            this.endText1.TabIndex = 1;
+            this.endText1.Text = "label2";
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.Location = new System.Drawing.Point(383, 119);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(35, 13);
+            this.scoreText.TabIndex = 2;
+            this.scoreText.Text = "label3";
+            // 
+            // endText2
+            // 
+            this.endText2.AutoSize = true;
+            this.endText2.Location = new System.Drawing.Point(446, 136);
+            this.endText2.Name = "endText2";
+            this.endText2.Size = new System.Drawing.Size(35, 13);
+            this.endText2.TabIndex = 3;
+            this.endText2.Text = "label4";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 15;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick_1);
+            // 
+            // Titan
+            // 
+            this.Titan.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Titan.Location = new System.Drawing.Point(170, 171);
+            this.Titan.Name = "Titan";
+            this.Titan.Size = new System.Drawing.Size(100, 50);
+            this.Titan.TabIndex = 4;
+            this.Titan.TabStop = false;
+            this.Titan.Click += new System.EventHandler(this.flappyBird_Click);
+            // 
+            // ground
+            // 
+            this.ground.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ground.Location = new System.Drawing.Point(-2, 316);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(806, 134);
+            this.ground.TabIndex = 5;
+            this.ground.TabStop = false;
+            // 
+            // pipeTop
+            // 
+            this.pipeTop.Location = new System.Drawing.Point(474, 62);
+            this.pipeTop.Name = "pipeTop";
+            this.pipeTop.Size = new System.Drawing.Size(100, 50);
+            this.pipeTop.TabIndex = 6;
+            this.pipeTop.TabStop = false;
+            // 
+            // pipeBottom
+            // 
+            this.pipeBottom.Location = new System.Drawing.Point(613, 158);
+            this.pipeBottom.Name = "pipeBottom";
+            this.pipeBottom.Size = new System.Drawing.Size(100, 50);
+            this.pipeBottom.TabIndex = 7;
+            this.pipeBottom.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pipeBottom);
+            this.Controls.Add(this.pipeTop);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.Titan);
+            this.Controls.Add(this.endText2);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.endText1);
+            this.Controls.Add(this.gameDesigner);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.inGameKeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gameKeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Titan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label gameDesigner;
+        private System.Windows.Forms.Label endText1;
+        private System.Windows.Forms.Label scoreText;
+        private System.Windows.Forms.Label endText2;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox Titan;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox pipeTop;
+        private System.Windows.Forms.PictureBox pipeBottom;
     }
 }
 
--- Internal/Form1.cs
+++ Internal/Form1.cs
@@ -12,12 +12,65 @@ namespace Internal
 {
     public partial class Form1 : Form
     {
+        bool jumping = false; int pipeSpeed = 5; int gravity = 5; int inscore = 0;
         public Form1()
         {
             InitializeComponent();
+            endText1.Text = "Game Over!"; gameDesigner.Text = "Game designed by your name here"; endText1.Visible = false; endText2.Visible = false;
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+        }
+
+        private void inGameKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space) { jumping = true; gravity = -5; }
+        }
+
+        private void gameKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space) { jumping = false; gravity = 5; }
+        }
+
+        private void endGame()
+        {
+            gameTimer.Stop();
+            endText1.Visible = true; endText2.Visible = true; endText2.Text = "Your Final Score is: " + inscore; gameDesigner.Visible = true;
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gameTimer_Tick_1(object sender, EventArgs e)
+        {
+
+            pipeBottom.Left -= pipeSpeed;
+            pipeTop.Left -= pipeSpeed; Titan.Top += gravity; scoreText.Text = " " + inscore;
+
+            if (Titan.Bounds.IntersectsWith(ground.Bounds))
+            {
+                Titan.Top += 0;
+            }
+            if (Titan.Bounds.IntersectsWith(pipeBottom.Bounds))
+            {
+                endGame();
+            }
+            if (Titan.Bounds.IntersectsWith(pipeTop.Bounds))
+            {
+                endGame();
+            }
+            if (pipeBottom.Left < -80)
+
+            { pipeBottom.Left = 1000; inscore += 1; }
+            else if (pipeTop.Left < -95)
+
+            { pipeTop.Left = 1100; inscore += 1; }
+        }
+
+        private void flappyBird_Click(object sender, EventArgs e)
         {
 
         }
